01174178501da308e241a895df036c81015987e1
Ivonei Balena <iibalena@gmail.com>
2026-02-11 13:28:27 -0300

    feat(api): add module imports for Config, Db, Repos, Webhooks, and Builds

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from '@shared/db/db.module';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { ReposModule } from './repos/repos.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
+import { BuildsModule } from './builds/builds.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DbModule,
+
+    ReposModule,
+    WebhooksModule,
+    BuildsModule,
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
