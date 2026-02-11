ca8153278a38589da5b002407b7688681b922ae7
Ivonei Balena <iibalena@gmail.com>
2026-02-11 13:28:43 -0300

    feat(api): add BuildsModule to AppModule imports

--- apps/node-project-builder/src/app.module.ts
+++ apps/node-project-builder/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReposModule } from './repos/repos.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
+import { BuildsModule } from './builds/builds.module';
 
 @Module({
-  imports: [ReposModule, WebhooksModule],
+  imports: [ReposModule, WebhooksModule, BuildsModule],
   controllers: [AppController],
   providers: [AppService],
 })
